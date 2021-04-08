a7f2c6c0fb112913498e36bb1eaf7a8e05994bdc
Riza Ramadan <riza.ramadan@gmail.com>
2021-04-08 20:12:03 +0700

    copying layout and home
    Just copying from readme, nothing special

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,30 +10,45 @@ type Props = {
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
     <Head>
-      <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <title>Conduit</title>
+      <link href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+      <link href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css" />
+      <link rel="stylesheet" href="//demo.productionready.io/main.css" />
     </Head>
     <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">conduit</a>
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              <a className="nav-link active" href="">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <i className="ion-compose"></i>&nbsp;New Post
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <i className="ion-gear-a"></i>&nbsp;Settings
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">Sign up</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
     {children}
     <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
+      <div className="container">
+        <a href="/" className="logo-font">conduit</a>
+        <span className="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+      </div>
     </footer>
   </div>
 )
